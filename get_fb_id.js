7da66d40a0695a01130711888a7e5f8c7522c290
Azat <robocop92_92@mail.ru>
2019-07-10 23:01:08 +0500

    first commit

---
+++ get_fb_id.js
@@ -0,0 +1,13 @@
+/** 
+  * SearchBook 
+  *
+  * @author sowdust<mattia.vinci@mediaservice.net>
+  * @url    https://github.com/sowdust/searchbook
+  * 
+  * License:  free
+  * 
+*/
+
+html = document.documentElement.innerHTML;
+console.log("Error while getting Facebook id");
+
